590d662db7ea672d467ab8572f76278d20ea9a55
sha ce <82579992+sha-ce@users.noreply.github.com>
2021-07-16 18:32:02 +0900

    a

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,59 +1,13 @@
 <template>
-  <div class="nav-wrap">
-    <div class="nav-container">
-      <div class="menu">
-        <div class="button" tabindex="0">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </div>
-        <div class="nav-content">
-          <ul>
-            <li>
-              <div class="home">
-                <a href="/">
-                  <font-awesome-icon class="icon" icon="home" />
-                  HOME
-                </a>
-              </div>
-            </li>
-            <li><p>作品を見る</p></li>
-            <li>
-              <div class="list">
-                <a href="linkUrl1">
-                  <font-awesome-icon class="icon" icon="gamepad" />
-                  GAME
-                </a>
-              </div>
-            </li>
-            <li>
-              <div class="list">
-                <a href="linkUrl2">
-                  <font-awesome-icon class="icon" :icon="['far', 'images']" />
-                  CG
-                </a>
-              </div>
-            </li>
-            <li>
-              <div class="list">
-                <a href="linkUrl3">
-                  <font-awesome-icon class="icon" icon="laptop-code" />
-                  HACK
-                </a>
-              </div>
-            </li>
-            <li>
-              <div class="list">
-                <a href="linkUrl4">
-                  <font-awesome-icon class="icon" icon="palette" />
-                  MEDIA_ART
-                </a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+  <div>
+    <div class="button" @click="ActiveBtn=!ActiveBtn">
+      <span class="icon-bar" :class="{'icon-bar':ActiveBtn}"></span>
+      <span class="icon-bar" :class="{'icon-bar':ActiveBtn}"></span>
+      <span class="icon-bar" :class="{'icon-bar':ActiveBtn}"></span>
     </div>
+    <transition name="slide">
+      <div class="change" v-show="ActiveBtn">この文字が切り替わるはず</div>
+    </transition>
   </div>
 </template>
 
@@ -61,6 +15,7 @@
 export default {
   data() {
     return {
+      ActiveBtn: false,
       linkUrl1: "/community/1",
       linkUrl2: "/community/2",
       linkUrl3: "/community/3",
@@ -71,113 +26,26 @@ export default {
 </script>
 
 <style scoped>
-.nav-wrap {
-  position: fixed;
-  margin-top: 30px;
-  z-index: 99;
-}
-.nav-container {
-  position: relative;
-}
 .button {
   position: absolute;
-  top: 20px;
-  left: 20px;
   height: 30px;
   width: 30px;
-  margin: 0;
-  z-index: 100;
-  -webkit-appearance: none;
-  border: 0;
-  border-radius: 0;
+  border: solid 1px black;
   cursor: pointer;
-  pointer-events: auto;
-  touch-action: manipulation;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  z-index: 100;
 }
-.icon-bar {
-  display: block;
-  width: max(4vh, 20px);
-  height: 2px;
-  background: lightgray;
-  transition: 0.3s;
-}
-.icon-bar + .icon-bar {
-  margin-top: 7px;
-}
-.nav-container:focus-within .button {
-  pointer-events: none;
-}
-.nav-container:focus-within .icon-bar:nth-of-type(1) {
-  transform: translate3d(0, 9px, 0) rotate(-45deg);
-}
-.nav-container:focus-within .icon-bar:nth-of-type(2) {
-  opacity: 0;
-}
-.nav-container:focus-within .icon-bar:nth-of-type(3) {
-  transform: translate3d(0, -9px, 0) rotate(45deg);
-}
-.nav-content {
-  padding: 100px 0 0 30px;
-  max-width: 300px;
+.change {
   position: absolute;
-  top: -30px;
-  left: 0;
-  width: 250px;
-  height: 100vh;
-  background: #4a4a4a;
-  pointer-events: auto;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  transform: translateX(-100%);
-  transition: transform 0.3s;
-  will-change: transform;
-  contain: paint;
-  flex-direction: column;
-  z-index: 99;
+  padding-top: 50px;
+  background-color: gray;
+  max-width: 80%;
 }
-.home {
-  margin-bottom: 10px;
-}
-.list {
-  padding-left: 20px;
-}
-.home,
-.list {
-  display: flex;
-}
-.icon {
-  color: white;
-  font-size: 15px;
-}
-.nav-content li a,
-.nav-content li p {
-  padding: 5px 5px;
-  display: block;
-  text-transform: uppercase;
-  transition: color 0.1s;
-  color: white;
+.slide-enter-active, .slide-leave-active {
+  transform: translate(0px, 0px); 
+  transition: transform 225ms cubic-bezier(0.2, 1, 0.2, 1) 0ms;
 }
 
-.nav-content li a:hover {
-  opacity: 0.6;
-}
-.nav-container:focus-within .nav-content {
-  transform: none;
-}
-a,
-a:visited,
-a:focus,
-a:active,
-a:link {
-  text-decoration: none;
-  outline: 0;
-}
-a {
-  color: currentColor;
-  transition: 0.2s ease-in-out;
-}
-ul {
-  padding: 0;
-  list-style: none;
+.slide-enter, .slide-leave-to {
+  transform: translateY(0) translateX(-100vh);
 }
 </style>
